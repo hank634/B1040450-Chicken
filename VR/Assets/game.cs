b078a75fd857443a36836ecd7cb5e4373ad47ad2
hank634 <55218499+hank634@users.noreply.github.com>
2019-12-26 02:53:13 +0800

    VR

---
+++ VR/Assets/game.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class game : MonoBehaviour {
+
+	//
+	public void ThrowPickUP()
+	{
+		print("撿起物件");
+	}
+	public void ThrowDetach(GameObject obj)
+	{
+		print("放開物件");
+
+		Destroy(obj);
+	}
+	public void ThrowHeld()
+	{
+		print("拿著物件");
+	}
+}
